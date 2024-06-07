efdf60940eba9a5c1ec75eccc126ed2965f59457
MAriaReneVillegas <villegasoshin25@gmail.com>
2024-06-06 20:32:15 -0400

    actualizando dto

--- src/main/java/com/backend/ClinicaOdontologicaApplication.java
+++ src/main/java/com/backend/ClinicaOdontologicaApplication.java
@@ -1,20 +1,25 @@
 package com.backend;
 
 import com.backend.repository.dbconnection.H2Connection;
-import com.backend.repository.impl.OdontologoDaoMemory;
-import jakarta.annotation.PostConstruct;
 import org.apache.log4j.Logger;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
-    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoMemory.class);
+    private static final Logger LOGGER = Logger.getLogger(ClinicaOdontologicaApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicaOdontologicaApplication.class, args);
         H2Connection.createTable();
         LOGGER.info("Application works :)");
-        LOGGER.info("Created by: Yessica Silver y Oshin Villegas :)");
+        LOGGER.info("Created by: Chacón Daniel & Soto Luis :)");
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
--- src/main/java/com/backend/repository/dbconnection/H2Connection.java
+++ src/main/java/com/backend/repository/dbconnection/H2Connection.java
@@ -37,4 +37,3 @@ public class H2Connection {
         }
     }
 }
-
